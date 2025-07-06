1bb86ab6c47563b661bb85021dadb08dba32cc7b
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-07-06 17:17:12 +0200

    add details product

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,6 @@
 import { useState, useMemo } from "react";
 import ProductQuantity from "../components/ProductQuantity.js";
+import { Link } from "react-router-dom";
 import "../css/Products.css";
 
 import oatmilkFirst from "../assets/products/oatmilk/oatmilk_1.jpg";
@@ -16,6 +17,10 @@ import bundleFirst from "../assets/products/bundle/bundle_1.jpg";
 import bundleSecond from "../assets/products/bundle/bundle_2.jpg";
 import bundleThird from "../assets/products/bundle/bundle_3.jpg";
 import bundleFourth from "../assets/products/bundle/bundle_4.jpg";
+
+// import detailsFirst from "../assets/details/inside_1.jpg";
+import detailsSecond from "../assets/details/inside_2.jpg";
+import detailsThird from "../assets/details/inside_3.jpg";
 
 const oatmilkImages = [
   oatmilkFirst,
@@ -69,8 +74,8 @@ function Products() {
     color === "oatmilk"
       ? oatmilkImages
       : color === "matcha"
-      ? matchaImages
-      : bundleImages;
+        ? matchaImages
+        : bundleImages;
   const currentProduct = products[color];
 
   /**
@@ -83,8 +88,8 @@ function Products() {
       selectedColor === "oatmilk"
         ? oatmilkImages[0]
         : selectedColor === "matcha"
-        ? matchaImages[0]
-        : bundleImages[0],
+          ? matchaImages[0]
+          : bundleImages[0],
     );
   };
 
@@ -179,6 +184,64 @@ function Products() {
           </div>
           <div className="product">
             <ProductQuantity product={currentProduct} />
+          </div>
+        </div>
+      </div>
+      <div className="section-divider"></div>
+      <div className="product-details-container">
+        <div className="product-details-image">
+          <img src={detailsSecond} alt="Product Details 2" />
+          <img src={detailsThird} alt="Product Details 3" />
+        </div>
+        <div className="product-details-text">
+          <div className="product-specifications">
+            <h3>Product Specifications</h3>
+            <p>
+              <strong>Size:</strong> A5 (148 x 210 mm)
+            </p>
+            <p>
+              <strong>Cover:</strong> Durable hardcover
+            </p>
+            <p>
+              <strong>Paper:</strong> 80 gsm, white paper
+            </p>
+            <p>
+              <strong>Pages:</strong> 160 total
+            </p>
+          </div>
+          <div className="inside-journal">
+            <h3>Inside the Journal</h3>
+            <p>Designed for 3 months of growth and clarity.</p>
+            <p>Structured in a repeating cycle:</p>
+            <ul>
+              <li>Monthly Goals</li>
+              <li>Weekly Plans</li>
+              <li>Weekly Reflections</li>
+              <li>Monthly Reflections</li>
+            </ul>
+            <p>Includes:</p>
+            <ul>
+              <li>Guided reflection prompts for deeper self-discovery</li>
+              <li>4 bonus templates for clarity, creativity, and focus</li>
+              <li>10 dedicated pages for meeting notes</li>
+              <li>
+                100+ dot grid pages for free writing, sketching, or
+                brainstorming
+              </li>
+              <li>Undated layout - start anytime, no wasted pages</li>
+            </ul>
+          </div>
+          <div className="additional-features">
+            <h3>Additional Features</h3>
+            <ul>
+              <li>Elastic closure band for secure, on-the-go journaling</li>
+              <li>
+                Name personalization available{" "}
+                <Link to="/personalize" className="bold-underline-link">
+                  here
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
